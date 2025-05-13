3b93d24e434ebd9065eb0445750d47fad275ce29
asinsayedali <asinsayedali008@gmail.com>
2025-05-13 17:10:33 +0530

    added delete todo endpoint

--- Backend/routers/todo.py
+++ Backend/routers/todo.py
@@ -17,3 +17,18 @@ def create_todo(db: SessionDep, request: models.TodoCreate,current_user: models.
     db.commit()
     db.refresh(todo)
     return todo
+
+@router.delete("/todos/{todo_id}", status_code=status.HTTP_202_ACCEPTED)
+def delete_todo(
+    todo_id: int,
+    db: SessionDep,
+    current_user: models.User = Depends(authentication.get_current_user),
+):
+    todo = db.exec(select(models.Todo).where(models.Todo.id == todo_id, models.Todo.user_id == current_user.id)).first()
+
+    if not todo:
+        raise HTTPException(status_code=404, detail="Todo not found")
+
+    db.delete(todo)
+    db.commit()
+    return  {"details":"Todo deleted"}
